0c45364d8cd02c7916eb7cbf9f7f344b30f3071a
MosqueraSt3 <esmosquera66@misena.edu.co>
2021-03-06 14:18:12 -0500

    component auth

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,8 +1,84 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
+
+    // STATE LOG IN
+    const [ user, setUser ] = useState({
+        email: '',
+        password: ''
+    })
+
+    // STATE ERROR
+    const [ error, setError ] = useState(false)
+
+    // GET USER
+    const { email, password } = user
+
+    // HANDLE CHANGE
+    const handleChange = e => {
+        setUser({
+            ...user,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    // HANDLE SUBMIT
+    const handleSubmit = e => {
+        e.preventDefault()
+
+        // NOT EMPTY FIELDS
+        if (email.trim() === '' || password.trim() === '') return setError(true)
+        
+        // SUCCESS
+        setError(false)
+
+
+    }
+
     return (
-        <h1>Login</h1>
+        <div className="form-usuario">
+            <div className="contenedor-form sombra-dark">
+                <h1>Log In</h1>
+                <form
+                    onSubmit={handleSubmit}
+                >
+                    <div className="campo-form">
+                        <label htmlFor="email">Email</label>
+                        <input 
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder="Email"
+                            value={email}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className="campo-form">
+                        <label htmlFor="password">Password</label>
+                        <input 
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Password"
+                            value={password}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className="campo-form">
+                        <input 
+                            type="submit"
+                            className="btn btn-primario btn-block"
+                            value="Log In"
+                        />
+                    </div>
+                </form>
+                <Link
+                    to={'/new-account'}
+                    className="enlace-cuenta"
+                >Sign Up</Link>
+            </div>
+        </div>
     )
 }
 
--- src/components/auth/NewAccount.js
+++ src/components/auth/NewAccount.js
@@ -1,8 +1,112 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const NewAccount = () => {
+
+    // STATE LOG IN
+    const [ user, setUser ] = useState({
+        name: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+    })
+
+    // STATE ERROR
+    const [ error, setError ] = useState(false)
+
+    // GET USER
+    const { name, email, password, confirmPassword } = user
+
+    // HANDLE CHANGE
+    const handleChange = e => {
+        setUser({
+            ...user,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    // HANDLE SUBMIT
+    const handleSubmit = e => {
+        e.preventDefault()
+
+        // NOT EMPTY FIELDS
+        if (email.trim() === '' || password.trim() === '') return setError(true)
+
+        // MIN CHARACTERS
+
+        // CONFIRM PASSWORD
+        
+        // SUCCESS
+        setError(false)
+
+
+    }
+
     return (
-        <h1>NewAccount</h1>
+        <div className="form-usuario">
+            <div className="contenedor-form sombra-dark">
+                <h1>Sign Up</h1>
+                <form
+                    onSubmit={handleSubmit}
+                >
+                    <div className="campo-form">
+                        <label htmlFor="name">Name</label>
+                        <input 
+                            type="text"
+                            id="name"
+                            name="name"
+                            placeholder="Name"
+                            value={name}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className="campo-form">
+                        <label htmlFor="email">Email</label>
+                        <input 
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder="Email"
+                            value={email}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className="campo-form">
+                        <label htmlFor="password">Password</label>
+                        <input 
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Password"
+                            value={password}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className="campo-form">
+                        <label htmlFor="confirm">Confirm Password</label>
+                        <input 
+                            type="password"
+                            id="confirm"
+                            name="confirmPassword"
+                            placeholder="Confirm Password"
+                            value={confirmPassword}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className="campo-form">
+                        <input 
+                            type="submit"
+                            className="btn btn-primario btn-block"
+                            value="Sign Up"
+                        />
+                    </div>
+                </form>
+                <Link
+                    to={'/'}
+                    className="enlace-cuenta"
+                >Go Back</Link>
+            </div>
+        </div>
     )
 }
 
